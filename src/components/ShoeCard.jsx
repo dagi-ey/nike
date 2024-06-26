2a6e2f571ba93fcf27401301574590850e566293
Dagmawi Eyob <dagieyob114@gmail.com>
2024-06-26 12:22:59 -0700

    added hero img

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,7 +1,16 @@
 const shoeCard = ({changeBigShoeImage, bigShoeImage, imgURL}) => {
+  const handleClick = () => {
+    if(bigShoeImage !== imgURL.bigShoe){
+      changeBigShoeImage(imgURL.bigShoe)
+    }
+  }
   return (
     <div className={`border-2 rounded-xl
-    ${bigShoeImage === imgURL? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}>
+    ${bigShoeImage === imgURL? 'border-coral-red' : 'border-transparent'}
+    cursor-pointer max-sm:flex-1`} onClick={handleClick}>
+       <div>
+    <img src={imgURL.thumbnail} alt="" />
+    </div>
     </div>
   )
 }
