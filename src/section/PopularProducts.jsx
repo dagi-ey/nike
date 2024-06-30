a6b2bda9157111310c45b9ea106a09abc36e1822
Dagmawi Eyob <dagieyob114@gmail.com>
2024-06-30 15:14:33 -0700

    popular product initiated

--- src/section/PopularProducts.jsx
+++ src/section/PopularProducts.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { products } from "../constants"
 
 const PopularProducts = () => {
   return (
-    <div>
-      PopularProducts
-    </div>
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5 ">
+
+      </div>
+    </section>
   )
 }
 
