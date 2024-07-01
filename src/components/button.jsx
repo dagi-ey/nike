fb17d9943b363ae3bd5bec6e16d04cf87b6648e7
Dagmawi Eyob <dagieyob114@gmail.com>
2024-07-01 12:55:45 -0700

    Special Offer added and fully done

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,8 +1,11 @@
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
     return (
-      <button className="flex justify-center items-center gap-2 px-7 py-4
-      border font-montserrat text-white text-lg leading-none bg-coral-red rounded-full
-      border-coral-red ">
+      <button  className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+        ${
+          backgroundColor
+            ? `${backgroundColor} ${textColor} ${borderColor}`
+            : "bg-coral-red text-white border-coral-red"
+        } rounded-full w-full"`}>
           {label} 
   
           {iconURL&&<img src={iconURL}
--- src/section/SpecialOffer.jsx
+++ src/section/SpecialOffer.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/button"
 const SpecialOffer = () => {
   return (
-    <div>
-       SpecialOffer 
-    </div>
+    <section className='flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img src={offer} width={773} height={687} 
+        className="object-contain w-full" />
+      </div>
+
+      <div className='flex flex-col flex-1'>
+        <h2 className="font-bold text-4xl capitalize font-palanquin lg:max-w-lg ">
+          <span className="text-coral-red">Special </span>
+          <span className="text-coral-red">Offer </span>
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">Discover The New Styles You Can WearDiscover 
+        The New Styles You Can Wear Discover The New Styles You Can Wear Discover The New Styles You Can Wear</p>
+        <p className="mt-4 lg:max-w-lg info-text">Discover The New Styles You Can WearDiscover 
+        The New Styles You Can Wear Discover</p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label='Shop now' iconURL={arrowRight} />
+          <Button
+            label='Learn more'
+            backgroundColor='bg-white'
+            borderColor='border-slate-gray'
+            textColor='text-slate-gray'
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
